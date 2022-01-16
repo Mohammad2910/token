138099dc084e4033d4657be6cf55b636a61d9143
didi <s147197@student.dtu.dk>
2022-01-16 20:31:25 +0100

    setup of methods and test for TokenManager.java

--- src/main/java/domain/TokenManager.java
+++ src/main/java/domain/TokenManager.java
@@ -1,6 +1,30 @@
 package domain;
 
+import domain.model.TokenSet;
+
 public class TokenManager {
+
+    private TokenGenerator generator = new TokenGenerator();
+
+    //has to communicate with storage
+    public boolean validateToken(String cid, String token){}
+
+    //this should generate a set of tokens specified by amount
+    public void generatetokens(int amount){}
+
+    //this should return a tokenset to the customer who requests some tokens
+    public TokenSet supplyTokens(){ return new TokenSet();}
+
+    //this should contact storage to see how many tokens a given customer has
+    private int checkCustomerTokenset(String cid){return 0;}
+
+    //this should store tokens in the db (we dont know if we should just store one or multiple at a time)
+    public void storeTokens(TokenSet tokens){}
+
+    //should remove a single token from the storage.
+    private void consumeToken(String cid, String Token){}
+
+
 
 
 }
---
+++ src/test/java/tokens/TokenManagerTest.java
@@ -0,0 +1,32 @@
+package tokens;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TokenManagerTest {
+
+
+    @Test
+    void validateToken(){}
+
+    @Test
+    void generateToken(){}
+
+    @Test
+    void supplyTokens(){
+    }
+
+    @Test
+    void checkCustomerTokenset(){
+    }
+
+    @Test
+    void storeTokens(){}
+
+    @Test
+    void consumeToken(){}
+
+
+
+}
