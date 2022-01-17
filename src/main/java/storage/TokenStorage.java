7fd4b693d077642ecbec80a920625a2c6d6d1e1c
didi <s147197@student.dtu.dk>
2022-01-17 15:28:41 +0100

    Added documentation of the TokenStorage class

--- src/main/java/storage/TokenStorage.java
+++ src/main/java/storage/TokenStorage.java
@@ -1,43 +1,53 @@
 package storage;
 
 import domain.model.TokenSet;
-
 import java.util.HashMap;
 
+/**
+ * Class that represents the storage unit of customer-tokenSet mappings
+ * @author Renjue, Christian and David
+ */
 public class TokenStorage {
 
     HashMap<String, TokenSet> tokenHashMap = new HashMap<>();
 
-    //adding a new customer
+    /**
+     * Adds a new customer-tokenSet mapping to the storage
+     * @param cid of the customer to add
+     * @param tokens that the customer possess
+     */
     public void addNewEntry(String cid, TokenSet tokens){
 
         tokenHashMap.put(cid, tokens);
     }
 
     /**
-     *
-     * @param cid
-     * @param tokens
+     * Method for adding a new set of tokens to a customers existing tokenSet
+     * @param cid of the customer
+     * @param tokens to add to the already existing tokenSet
      */
-    //adding new tokens to an already existing customer
     public void addTokens(String cid, TokenSet tokens){
 
         TokenSet tokenSetFromStorage = tokenHashMap.get(cid);
         for (String token: tokens.getSet()) {tokenSetFromStorage.add(token);}
     }
 
-    //removes a token from
+
+    /**
+     * Method for removing a token from a specified customers tokenSet
+     * @param cid of the customer
+     * @param token to be removed from the customer's tokenSet
+     */
     public void removeTokenFromCustomer(String cid, String token){
         TokenSet set = tokenHashMap.get(cid);
         set.remove(token);
     }
 
     /**
-     *
-     * @param cid
-     * @return
+     * Method for getting the amount of tokens in a specified customer's tokenSet
+     * @param cid of the customer
+     * @return Integer specifying the amount of tokens in the tokenSet
      */
-    //return the size of a specified customer's tokenset
     public int getCustomerTokenSetSize(String cid){
 
        TokenSet tokenSet = tokenHashMap.get(cid);
@@ -45,27 +55,19 @@ public class TokenStorage {
     }
 
     /**
-     *
-     * @param cid
-     * @param token
-     * @return
+     * Method for checking if a specified token exists in a specified customer's tokenSet
+     * @param cid of the customer
+     * @param token to be checked
+     * @return boolean stating whether the specified token did exist or not
      */
-    //checks if a specified token exists
     public boolean isCustomerTokenValid(String cid, String token){
         TokenSet set = tokenHashMap.get(cid);
         return set.searchForToken(token);
     }
 
     /**
-     *
-     * @return
+     * Method for getting the whole hashmap (storage) of all customer-tokenSet mappings
+     * @return Hashmap (storage)
      */
-    //getter for the whole hashmap (storage)
     public HashMap<String, TokenSet> getTokenHashMap(){return tokenHashMap;}
-
-
-
-
-
-
 }
