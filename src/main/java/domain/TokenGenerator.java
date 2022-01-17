d155b468e1e826f03a6dd8aaeb08c0fa78539b31
didi <s147197@student.dtu.dk>
2022-01-17 19:33:59 +0100

    small comment

--- src/main/java/domain/TokenGenerator.java
+++ src/main/java/domain/TokenGenerator.java
@@ -11,7 +11,7 @@ public class TokenGenerator {
     SecureRandom RNG = new SecureRandom();
 
     /**
-     * Method that generates a single token of 32 bytes
+     * Method that generates a single token of 32 bytes from a RNG (Random Number Generator)
      * @return token as a string
      */
     public String generate(){
