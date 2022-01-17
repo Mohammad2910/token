888fe0678bfb11c3b078835d6ae85467756327ea
Renjue823 <mengsrjzoeee@gmail.com>
2022-01-17 21:33:03 +0100

    token manager implementation and tests

--- src/main/java/domain/TokenManager.java
+++ src/main/java/domain/TokenManager.java
@@ -10,6 +10,18 @@ public class TokenManager {
     private IStorageAdapter adapter = new StorageAdapter();
 
     /**
+     * Method for checking if the specified customer has had a tokenSet in storage
+     * @param cid of the specified customer
+     * @return boolean stating if the specified customer has had a tokenSet in storage or not
+     */
+    public boolean checkCustomerTokenSet(String cid){
+        if(adapter.getExternalStorage().getTokenHashMap().get(cid) != null){
+            return true;
+        }
+        return false;
+    }
+
+    /**
      * Method for adding a new customer with a specified tokenSet
      * @param cid of the customer to add
      * @param tokens that the customer possess
@@ -23,11 +35,31 @@ public class TokenManager {
         return false;
     }
 
-    //has to communicate with storage
-    public boolean validateToken(String cid, String token){return false;}
+    /**
+     * Method for validating a specified customer with a specified token
+     * @param cid of the specified customer
+     * @param token that the customer possess
+     * @return boolean stating whether the possessed token is valid or not
+     */
+    public boolean validateToken(String cid, String token){
+        if(adapter.getExternalStorage().getTokenHashMap().get(cid) == null){
+            return false;
+        }
+        return adapter.storageCheckCustomerToken(cid, token);
+    }
 
-    //this should generate a set of tokens specified by amount
-    public void generatetokens(int amount){}
+    /**
+     * Method for generating a tokenSet specified by amount
+     * @param amount of tokens to be generated
+     * @return a tokenSet specified by amount
+     */
+    public TokenSet generateTokens(int amount){
+        TokenSet set = new TokenSet();
+        for(int i = 0; i < amount; i++){
+            set.add(generator.generate());
+        }
+        return set;
+    }
 
     //this should return a tokenset to the customer who requests some tokens
     public TokenSet supplyTokens(){ return new TokenSet();}
@@ -36,7 +68,9 @@ public class TokenManager {
     private int checkCustomerTokensetSize(String cid){return 0;}
 
     //this should store tokens in the db (we dont know if we should just store one or multiple at a time)
-    public void storeTokens(TokenSet tokens){}
+    public void storeTokens(String cid, TokenSet tokens){
+        adapter.storageStoreTokens(cid, tokens);
+    }
 
     //should remove a single token from the storage.
     private void consumeToken(String cid, String Token){}
--- src/test/java/tokens/TokenManagerTest.java
+++ src/test/java/tokens/TokenManagerTest.java
@@ -15,6 +15,15 @@ class TokenManagerTest {
     TokenManager manager = new TokenManager();
 
     @Test
+    void checkCustomerTokenSet(){
+        String cid = "cid1";
+        TokenSet set = new TokenSet();
+        manager.addNewCustomer(cid, set);
+        assertTrue(manager.checkCustomerTokenSet(cid));
+        assertFalse(manager.checkCustomerTokenSet("cid2"));
+    }
+
+    @Test
     void addNewCustomer(){
         String cid = "cid1";
         TokenSet set = new TokenSet();
@@ -23,26 +32,41 @@ class TokenManagerTest {
         assertTrue(manager.addNewCustomer(cid, set));
         //customer has existed in storage
         assertFalse(manager.addNewCustomer(cid,set));
-
     }
 
 
     @Test
-    void validateToken(){}
+    void validateToken(){
+        String cid = "cid1";
+        String token = "token2";
+        TokenSet set = new TokenSet();
+        set.add("token1");
+        set.add(token);
+        set.add("token3");
+        manager.addNewCustomer(cid, set);
+        assertTrue(manager.validateToken(cid, token));
+        assertFalse(manager.validateToken(cid, "token4"));
+        assertFalse(manager.validateToken("cid2", token));
+    }
 
     @Test
-    void generateToken(){}
+    void generateToken(){
+        int amount = 3;
+        assertEquals(3, manager.generateTokens(amount).numberOfTokens());
+    }
 
     @Test
     void supplyTokens(){
     }
 
     @Test
-    void checkCustomerTokenset(){
-    }
+    void storeTokens(){
+        String cid = "cid1";
+        TokenSet set = new TokenSet();
+        manager.storeTokens(cid, set);
+        // assertNotNull();
 
-    @Test
-    void storeTokens(){}
+    }
 
     @Test
     void consumeToken(){}
